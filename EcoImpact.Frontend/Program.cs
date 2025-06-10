a69a3b0b86099b040a4164000a653a33e6cbd192
Bernardo Lima <1221977@isep.ipp.pt>
2025-06-11 00:12:09 +0100

    .

--- EcoImpact.Frontend/Program.cs
+++ EcoImpact.Frontend/Program.cs
@@ -5,9 +5,16 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7020/") }); 
+// Configurar appsettings com base no ambiente
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{builder.HostEnvironment.Environment}.json", optional: true, reloadOnChange: true);
+
+// Usa o valor da configuração para a base URL da API
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"];
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<IAuthService, AuthService>(); 
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 await builder.Build().RunAsync();
